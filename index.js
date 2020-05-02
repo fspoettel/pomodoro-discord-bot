79e888813b0b0f807a026c597ef36842a8de6778
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2020-05-02 17:18:31 +0200

    Reuse db client across queries, rename env url

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 /* eslint-disable func-call-spacing */
 require('dotenv').config()
 const Sentry = require('@sentry/node')
-const { withDb } = require('./lib/db')
+const { initDb, withDb } = require('./lib/db')
 const { init: initBot } = require('./bot')
 
-const { SENTRY_DSN } = process.env;
+const { NODE_ENV, SENTRY_DSN } = process.env;
 
 (async function () {
   Sentry.init({ dsn: SENTRY_DSN })
 
   try {
+    await initDb()
     await initBot()
     const count = await withDb((db, client) =>
       db.collection('timers').estimatedDocumentCount()
@@ -18,6 +19,10 @@ const { SENTRY_DSN } = process.env;
 
     console.log(`Timers in database: ${count}`)
   } catch (err) {
+    if (NODE_ENV !== 'production') {
+      console.error(err)
+    }
+
     Sentry.captureException(err)
 
     Sentry.close(5000).then(() => {
