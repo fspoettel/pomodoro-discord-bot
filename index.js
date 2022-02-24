c097de0625abbd4852dcd1ecb939af0e37fe101a
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2022-02-24 13:11:08 +0100

    chore: deploy to fly

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 /* eslint-disable no-unexpected-multiline */
 /* eslint-disable func-call-spacing */
+const http = require('http')
 require('dotenv').config()
 const Sentry = require('@sentry/node')
 const { initDb, withDb } = require('./lib/db')
 const { init: initBot } = require('./bot')
 
-const { NODE_ENV, SENTRY_DSN } = process.env;
+const { NODE_ENV, PORT, SENTRY_DSN } = process.env;
 
 (async function () {
   Sentry.init({ dsn: SENTRY_DSN })
@@ -17,7 +18,15 @@ const { NODE_ENV, SENTRY_DSN } = process.env;
       db.collection('timers').estimatedDocumentCount()
     )
 
-    console.log(`Timers in database: ${count}`)
+    const server = http.createServer((req, res) => {
+      res.setHeader('Content-type', 'text/plain')
+      res.writeHead(200)
+      res.end('ok')
+    })
+
+    server.listen(PORT, () => {
+      console.log(`Timers in database: ${count}`)
+    })
   } catch (err) {
     if (NODE_ENV !== 'production') {
       console.error(err)
